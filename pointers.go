4812da617432bcf7bc5367096b5373f99a4c1485
Thor Erik Lie <thor@otc.nu>
2016-01-13 07:42:29 +0100

    pointers

---
+++ pointers.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func main() {
+	x := 1
+	i := 5
+	fmt.Println("initial:", i)
+
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &x)
+	fmt.Println("pointer:", &i)
+}
